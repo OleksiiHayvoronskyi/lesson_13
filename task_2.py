48b7dbaf21192cfa392289050d7711271ac211a3
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-01-29 00:52:16 +0200

    Rewrite code to count array's average time

--- task_2.py
+++ task_2.py
@@ -13,32 +13,31 @@ print('\n--- Task 2. BUBBLE SORT ---')
 int_list = []
 
 # Кількість елементів для пошуку.
-for i in range(0, 5000):            # Діпазон пошуку
+for i in range(0, 300):            # Діпазон пошуку
     int_list.append(random.randint(1, 1000))
-print('Unsorted list:', int_list)
+print('Unsorted list:', int_list, '\n')
 
 
 # Функція Бульбашкового алгоритму сортування.
-def bubble_sort(nums):
-    # Закінчиться, коли відсортує список.
-    swapped = True
-    while swapped:
-        # Буде шукати, поки не перевірить весь список.
-        swapped = False
-        for i in range(len(nums) - 1):
-            if nums[i] > nums[i + 1]:
-                nums[i], nums[i + 1] = nums[i + 1], nums[i]
-                swapped = True
-        if not swapped:
-            break
-    print('Sorted list:', int_list)
+def bubble_sort(int_list, print_step=False):
+    n = len(int_list)
+    # Буде шукати, поки не перевірить весь список.
+    for i in range(n - 1):
+        for j in range(n - 1, i, -1):
+            if int_list[j] < int_list[j - 1]:
+                int_list[j], int_list[j - 1] = int_list[j - 1], int_list[j]
+            # Друкує кожен масив даних, що у діапазоні пошуку.
+            elif print_step:
+                print(f"array = {i}: ", end='')
+                start_time = time.time()
+                print(f'{int_list}')
+                print('Array execution time:',
+                      (time.time() - start_time) / len(int_list))
 
 
 start_time = time.time()
-
 # Запуск функції.
-bubble_sort(int_list)
+bubble_sort(int_list, True)
+print('\nSorted list:', int_list)
 print('Amount of numbers in the list:', len(int_list))
 print('The execution time is %s seconds.' % (time.time() - start_time))
-print('The average execution time of each number:',
-      (time.time() - start_time) / len(int_list))
