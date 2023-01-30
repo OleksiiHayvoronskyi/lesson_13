2b081f6741913522632b6df50570d5f7882604ad
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-01-30 20:33:07 +0200

    final option

--- task_2.py
+++ task_2.py
@@ -13,7 +13,7 @@ print('\n--- Task 2. BUBBLE SORT ---')
 int_list = []
 
 # Кількість елементів для пошуку.
-for i in range(0, 300):            # Діпазон пошуку
+for i in range(0, 200):            # Діпазон пошуку
     int_list.append(random.randint(1, 1000))
 print('Unsorted list:', int_list, '\n')
 
@@ -21,23 +21,24 @@ print('Unsorted list:', int_list, '\n')
 # Функція Бульбашкового алгоритму сортування.
 def bubble_sort(int_list, print_step=False):
     n = len(int_list)
-    # Буде шукати, поки не перевірить весь список.
-    for i in range(n - 1):
-        for j in range(n - 1, i, - 1):
-            if int_list[j] < int_list[j - 1]:
-                int_list[j], int_list[j - 1] = int_list[j - 1], int_list[j]
+    # Кільткість сортувань для заданого діапазону чисел.
+    for i in range(n):
+        sorted = False
+        for j in range(0, n - i - 1):
+            if int_list[j] > int_list[j + 1]:
+                int_list[j], int_list[j + 1] = int_list[j + 1], int_list[j]
+                sorted = True
+        if not sorted:
+            break
             # Друкує кожен масив даних, що у діапазоні пошуку.
-            elif print_step:
-                print(f"array = {i}: ", end='')
-                start_time = time.time()
-                print(f'{int_list}')
-                print('Array execution time:',
-                      (time.time() - start_time) / len(int_list))
+        elif print_step:
+            print(f"array = {i}: ", end='')
+            print(f'{int_list}')
+    print('\nSorted list:', int_list)
+    print('Amount of numbers in the list:', len(int_list))
 
 
 start_time = time.time()
 # Запуск функції.
 bubble_sort(int_list, True)
-print('\nSorted list:', int_list)
-print('Amount of numbers in the list:', len(int_list))
 print('The execution time is %s seconds.' % (time.time() - start_time))
