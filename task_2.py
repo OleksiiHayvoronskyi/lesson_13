0e38e530692f63973e027daac84c1142d51f4081
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-02-01 17:49:59 +0200

    done task 2

--- task_2.py
+++ task_2.py
@@ -9,36 +9,101 @@ random.seed(10)
 
 print('\n--- Task 2. BUBBLE SORT ---')
 
-# Список, куди будуть сортуватися числа.
-int_list = []
+# Списки, куди будуть сортуватися числа.
+int_list_1 = []
+int_list_2 = []
+int_list_3 = []
 
 # Кількість елементів для пошуку.
-for i in range(0, 200):            # Діпазон пошуку
-    int_list.append(random.randint(1, 1000))
-print('Unsorted list:', int_list, '\n')
+for i in range(0, 1000):  # Діпазон пошуку
+    int_list_1.append(random.randint(1, 1000))
+    int_list_2.append(random.randint(1, 1000))
+    int_list_3.append(random.randint(1, 1000))
 
 
 # Функція Бульбашкового алгоритму сортування.
-def bubble_sort(int_list, print_step=False):
-    n = len(int_list)
-    # Кільткість сортувань для заданого діапазону чисел.
-    for i in range(n):
+def bubble_sort(int_list_1, int_list_2, int_list_3, print_step=False):
+    n_1 = len(int_list_1)
+    n_2 = len(int_list_2)
+    n_3 = len(int_list_3)
+
+    # Масив 1.
+    print('Array 1:')
+    start_time = time.time()
+    print('Unsorted array 1:', int_list_1)
+    for i in range(n_1):
         sorted = False
-        for j in range(0, n - i - 1):
-            if int_list[j] > int_list[j + 1]:
-                int_list[j], int_list[j + 1] = int_list[j + 1], int_list[j]
+        for j in range(0, n_1 - i - 1):
+            if int_list_1[j] > int_list_1[j + 1]:
+                int_list_1[j], int_list_1[j + 1] = int_list_1[j + 1], \
+                    int_list_1[j]
                 sorted = True
         if not sorted:
             break
-            # Друкує кожен масив даних, що у діапазоні пошуку.
-        elif print_step:
-            print(f"array = {i}: ", end='')
-            print(f'{int_list}')
-    print('\nSorted list:', int_list)
-    print('Amount of numbers in the list:', len(int_list))
+        # Крок сортування.
+        # elif print_step:
+        #     start_time = time.time()
+        #     print(f"sorting = {i}: ", end='')
+        #     print(f'{int_list_1}', end='')
+        #     print(': time is %s seconds.' % (time.time() - start_time))
+
+    print('Sorted array 1:', int_list_1)
+    print('Amount of numbers in the array 1:', len(int_list_1))
+    print('Time is %s seconds.' % (time.time() - start_time),
+          '\n')
+
+    # Масив 2.
+    print('Array 2:')
+    start_time = time.time()
+    print('Unsorted array 2:', int_list_2)
+    for i in range(n_2):
+        sorted = False
+        for g in range(0, n_2 - i - 1):
+            if int_list_2[g] > int_list_2[g + 1]:
+                int_list_2[g], int_list_2[g + 1] = int_list_2[g + 1], \
+                    int_list_2[g]
+                sorted = True
+        if not sorted:
+            break
+        # Крок сортування.
+        # elif print_step:
+        #     start_time = time.time()
+        #     print(f"sorting = {i}: ", end='')
+        #     print(f'{int_list_2}', end='')
+        #     print(': time is %s seconds.' % (time.time() - start_time))
+
+    print('Sorted array 2:', int_list_2)
+    print('Amount of numbers in the list 2:', len(int_list_2))
+    print('Time is %s seconds.' % (time.time() - start_time),
+          '\n')
+
+    # Масив 3.
+    print('Array 3:')
+    start_time = time.time()
+    print('Unsorted array 3:', int_list_3)
+    for i in range(n_3):
+        sorted = False
+        for d in range(0, n_3 - i - 1):
+            if int_list_3[d] > int_list_3[d + 1]:
+                int_list_3[d], int_list_3[d + 1] = int_list_3[d + 1], \
+                    int_list_3[d]
+                sorted = True
+        if not sorted:
+            break
+        # Крок сортування.
+        # elif print_step:
+        #     start_time = time.time()
+        #     print(f"sorting = {i}: ", end='')
+        #     print(f'{int_list_3}', end='')
+        #     print(': time is %s seconds.' % (time.time() - start_time))
+
+    print('Sorted array 3:', int_list_3)
+    print('Amount of numbers in the list 3:', len(int_list_3))
+    print('Time is %s seconds.' % (time.time() - start_time),
+          '\n')
 
 
 start_time = time.time()
 # Запуск функції.
-bubble_sort(int_list, True)
-print('The execution time is %s seconds.' % (time.time() - start_time))
+bubble_sort(int_list_1, int_list_2, int_list_3, True)
+print('The total execution time is %s seconds.' % (time.time() - start_time))
